1b83cfacd3aba99cc5f1ac6fc4f1c305a6376d08
mystica2000 <mysticainf@gmail.com>
2024-09-19 01:55:04 +0530

    feat: signalling using ws, clean up, send audio to backend

--- frontend/index.js
+++ frontend/index.js
@@ -1,19 +1,88 @@
+let ws;
+
 function initEvents() {
     const recordAudioBtn = document.getElementById("record");
     const streamAudioBtn = document.getElementById("stream");
+    const streaming = document.querySelector(".streaming");
+    const recording = document.querySelector(".recording");
+
 
     recordAudioBtn.addEventListener("click", () => {
-        console.log("record audio clicked")
+        console.log("record audio clicked");
+
+        if (!streaming.classList.contains("hide")) {
+            streaming.classList.toggle("hide");
+        }
+
+        recording.classList.remove("hide");
+
+
+        const permissionbtn = document.getElementById("permission");
+        permissionbtn.addEventListener("click", async () => {
+            await this.askPermissionForAudio();
+        })
+
     })
 
     streamAudioBtn.addEventListener("click", () => {
         console.log("stream audio clicked")
+
+        if (!recording.classList.contains("hide")) {
+            recording.classList.toggle("hide");
+        }
+
+        streaming.classList.remove("hide");
     })
+}
+
+async function askPermissionForAudio() {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    console.log("AUDIO GOT IT!!", stream);
+
+    const pc = new RTCPeerConnection();
+
+    pc.onicecandidate = async (event) => {
+        if (event.candidate == null) {
+            await pc.setLocalDescription(event.candidate);
+            console.log(pc.localDescription);
+            ws.send(btoa(JSON.stringify(pc.localDescription)));
+        }
+        console.log("ICE CANDIDATE!!", event);
+    }
+
+    stream.getAudioTracks().forEach((track) => {
+        pc.addTrack(track, stream);
+    });
+
+    ws.onmessage = async (event) => {
+        try {
+            const message = event.data;
+            const sdp = JSON.parse(atob(message));
+
+            if (sdp.type == 'answer') {
+                await pc.setRemoteDescription(new RTCSessionDescription(sdp));
+
+                const element = document.getElementById("video1");
+                element.muted = false;
+            }
+
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    try {
+        const offer = await pc.createOffer();
+        await pc.setLocalDescription(offer);
+    } catch (e) {
+        console.log("Offer failed", e);
+    }
+
 }
 
 async function isServerUp() {
     try {
-        const response = await fetch("http://localhost:8080");
+        const response = await fetch("http://localhost:8080/server_test");
         if (!response.ok) {
             throw new Error('Request failed', response.status)
         }
@@ -25,6 +94,13 @@ async function isServerUp() {
 
 async function initializeWebSocketServer() {
     if (await isServerUp()) {
+        console.log("Success");
+
+        ws = new WebSocket("ws://localhost:8080/ws");
+
+        ws.onerror = (e) => {
+            console.error(e);
+        }
 
     }
 }
--- main.go
+++ main.go
@@ -2,40 +2,88 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"github.com/pion/interceptor"
 	"github.com/pion/webrtc/v4"
 )
 
-type WebRTCMessage struct {
-	Type string // "offer" | "answer" | "ice-candidate"
-	Message *webrtc.SessionDescription // base64 SDP Message
-}
-
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
-		return r.Header.Get("origin") == "http://localhost:5000"
+		return true;
 	},
 }
 
+var pc *webrtc.PeerConnection
+
+func initializeWebRTCPeer() error {
+	if pc == nil {
+		fmt.Println("Initializing Peer Connection");
+
+		mediaEngine := &webrtc.MediaEngine{}
+
+		if err := mediaEngine.RegisterDefaultCodecs(); err != nil {
+			return err
+		}
+
+		i := &interceptor.Registry{}
+
+		if err := webrtc.RegisterDefaultInterceptors(mediaEngine, i); err != nil {
+			return err
+		}
+
+		api := webrtc.NewAPI(webrtc.WithMediaEngine(mediaEngine), webrtc.WithInterceptorRegistry(i))
+
+		var err error;
+		pc, err = api.NewPeerConnection(webrtc.Configuration{})
+
+		if err != nil {
+			return err
+		}
+
+		if _, err = pc.AddTransceiverFromKind(webrtc.RTPCodecTypeAudio); err != nil {
+			return err
+		}
+
+		pc.OnTrack(func(tr *webrtc.TrackRemote, r *webrtc.RTPReceiver) {
+			codec := tr.Codec()
+			fmt.Print(codec);
+		});
+
+	}
+
+	return nil
+}
+
+
 func decodeBase64ToSDP(str string, obj *webrtc.SessionDescription) {
+	fmt.Println("decoding...");
+
 	b, err := base64.StdEncoding.DecodeString(str);
 
 	if err != nil {
 		log.Print("error on decodeBase64 ",err);
 	}
 
-	fmt.Print(b);
+	if err = json.Unmarshal(b, obj); err != nil {
+		panic(err)
+	}
 
 }
 
-func handleByMessageType(message WebRTCMessage) {
-	if message.Type == "offer" {
+func encodeSDPtoBase64(obj *webrtc.SessionDescription) string {
+	fmt.Println("encoding...");
 
+	b, err := json.Marshal(obj)
+	if err != nil {
+		panic(err)
 	}
+
+	return base64.StdEncoding.EncodeToString(b)
 }
 
 func serverWs(w http.ResponseWriter, r *http.Request) {
@@ -49,31 +97,83 @@ func serverWs(w http.ResponseWriter, r *http.Request) {
 	defer c.Close()
 
 	for {
-		var webrtcMessage WebRTCMessage
-		err := c.ReadJSON(&webrtcMessage);
+
+
+
+		mt, message, err := c.ReadMessage();
 
 		if err != nil {
 			log.Println("read error: ",err);
 			break;
 		}
 
-		log.Println("recv message :",webrtcMessage);
-		handleByMessageType(webrtcMessage);
+		offer := webrtc.SessionDescription{}
+		decodeBase64ToSDP(string(message), &offer);
+
+		initializeWebRTCPeer();
+
+		err = pc.SetRemoteDescription(offer)
+		if err != nil {
+			panic(err)
+		}
+
+		answer, err := pc.CreateAnswer(nil);
+
+		if err != nil {
+			panic(err)
+		}
+
+		// similar to onIceCandidate call on the browser
+		gatherComplete := webrtc.GatheringCompletePromise(pc)
+
+		err = pc.SetLocalDescription(answer)
+		if err != nil {
+			panic(err)
+		}
+
+		<-gatherComplete
+
+		encodedSDP := encodeSDPtoBase64(pc.LocalDescription());
+		c.WriteMessage(mt, []byte(encodedSDP))
+
+		//handleByMessageType(webrtcMessage);
+	}
+}
+
+func CORS(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+		next(w,r)
 	}
 }
 
 func main()  {
 
-	http.HandleFunc("/server_test", func(w http.ResponseWriter, r *http.Request) {
+	err := initializeWebRTCPeer()
+	if err != nil {
+        log.Fatal("Failed to initialize WebRTC peer:", err)
+    }
+
+	defer func ()  {
+		if closeError := pc.Close(); closeError!=nil {
+			fmt.Printf("cannot close peerconnection %v\n", closeError);
+		}
+	}()
+
+	http.HandleFunc("/server_test", CORS(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Server is running"));
-	});
+	}));
 
 	http.HandleFunc("/ws", serverWs);
 
 	fmt.Println("Server is running at http://localhost:8080/");
-	err := http.ListenAndServe("0.0.0.0:8080",nil)
+	httpServerErr := http.ListenAndServe("0.0.0.0:8080",nil)
 	if err != nil {
-		panic(err)
+		panic(httpServerErr)
 	}
 
 }
