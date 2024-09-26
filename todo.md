259abc259b68640b78a5d29e249259508bc24c8c
mystica2000 <mysticainf@gmail.com>
2024-09-27 01:04:51 +0530

    feat: frontend get recordings

--- todo.md
+++ todo.md
@@ -1,9 +1,9 @@
 Todo
 
-- /recordings - GET ALL FILENAMES
-- /recordings/:id - DELETE FILENAME & REGENERATE FULLAUDIO (MAKE SURE FRONTEND IS ON LOADING STATE)
+- /recordings - GET ALL FILENAMES [done]
+- /recordings/:id - DELETE FILENAME & REGENERATE FULLAUDIO (MAKE SURE FRONTEND IS ON LOADING STATE) [doing]
 - /ws&mode=listen -> listen audio (filename will be passed!) -> pause/resume/switch
-- /ws&mode=record -> record audio & ffmpeg integrate
+- /ws&mode=record -> record audio & ffmpeg integrate [refactor!!!!]
 
 pause - make note of when stopped
 resume - start from where paused
--- webrtc-ffmpeg/src/layouts/Stream.tsx
+++ webrtc-ffmpeg/src/layouts/Stream.tsx
@@ -1,7 +1,29 @@
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
 import ServerLogs from "./ServerLogs";
+import { useEffect, useState } from "react";
 
 export default function Stream() {
+
+    const [recordings, setRecordings] = useState<String[]>([]);
+
+    const fetchRecordings = async () => {
+
+        try {
+            const fetchUrl = new URL('http://localhost:8080/recordings/');
+            const result = await fetch(fetchUrl.href);
+            const response = (await result.json());
+            console.log(response.recordings);
+
+            setRecordings(response.recordings);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchRecordings();
+    }, []);
+
     return <Card className="h-full flex flex-col">
         <CardHeader className="flex-none">
             <CardTitle>View Recordings</CardTitle>
@@ -9,8 +31,20 @@ export default function Stream() {
                 contains Recordings list
             </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-2 grow">
-            Hello
+        <CardContent className="space-y-2 grow overflow-auto border-solid border-neutral-100 border-2 rounded-md p-2 m-4">
+            <section className="">
+                {
+                    recordings && recordings.map((aRecording) => (
+                        <div className="flex">
+                            <span className="grow">{aRecording}</span>
+                            <div className="flex gap-3">
+                                <button className="rounded-md border-solid border-slate-400 border-2 m-1 p-1">play</button>
+                                <button className="rounded-md border-solid border-slate-400 border-2 m-1 p-1">delete</button>
+                            </div>
+                        </div>
+                    ))
+                }
+            </section>
         </CardContent>
         <CardFooter className="block grow-0 h-64">
             <ServerLogs />
